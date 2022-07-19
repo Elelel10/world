c8d37685b873352fa60e2804a5b64f9cd13e9890
nahyeon <nah1116@naver.com>
2022-07-19 18:05:02 +0900

    add day11

---
+++ Begin/src/day11/HashSetTest2.java
@@ -0,0 +1,30 @@
+package day11;
+import java.util.*;
+public class HashSetTest2 {
+
+	public static void main(String[] args) {
+		//Member 객체 3개 생성해서 HashSet에 저장하세요
+		Member m1=new Member("홍길동",20);
+		Member m2=new Member("고길동",25);
+		Member m3=new Member("최길동",30);
+		Member m4=new Member("홍길동",20);
+		Member m5=new Member("홍길동",30);
+		Set<Member>set=new HashSet<>();
+		set.add(m1);
+		set.add(m2);
+		set.add(m3);
+		set.add(m4);
+		set.add(m5);
+		System.out.println("set.size(): "+set.size());
+		//iterator()메소드로 저장된 회원들의 정보를 출력하세요
+		Iterator<Member>it=set.iterator();
+		for(int i=0;it.hasNext();i++) {
+			Member user=it.next();
+			System.out.println(i+": "+user.name+", "+user.age+"세");
+		}
+		System.out.println("m1.hashCode(): "+m1.hashCode());
+		System.out.println("m4.hashCode(): "+m4.hashCode());
+		System.out.println("m2.hashCode(): "+m2.hashCode());
+	}
+
+}
---
+++ Begin/src/day11/Local.java
@@ -0,0 +1,38 @@
+package day11;
+/*Local Inner class: 메소드 블럭 안에 클래스를 구성한 경우
+ * 	[1] 이름 있는 로컬 이너 클래스
+ * 	[2] 이름 없는 로컬 이너 클래스
+ * */
+public class Local {
+	String str="멤버변수@@@";
+	
+	public void sub() {
+		double pi=3.14;//지역변수(local var)
+		System.out.println("str: "+str);
+		System.out.println("pi: "+pi);
+		
+		class LocalInner{//local inner class
+			String lstr="LocalInner의 멤버변수###";
+			
+			void foo() {
+				//str
+				System.out.println("str: "+str);
+				//lstr
+				System.out.println("lstr: "+lstr);
+				//pi
+				System.out.println("pi: "+pi);
+				//pi=5.12;//error발생
+				/*로컬이너 클래스 안에서는 final이 아닌 지역변수는 접근 불가*/
+			}
+		}////////////////
+		/*로컬이너클래스는 자기가 속한 메소드 안에서 객체생성해야 함
+		 * 또한 클래스가 구성된 후에 객체 생성이 가능함*/
+	LocalInner li=new LocalInner();
+	li.foo();
+	}//sub()-------------
+	public static void main(String[] args) {
+		//LocalInner의 foo()호출하기[x]
+		Local l1=new Local();
+		l1.sub();
+	}
+}
